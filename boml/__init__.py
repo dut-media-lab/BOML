8624644cb33499861aa6d20586ffce46d9caf828
bmlsoc <bml_soc@163.com>
2020-08-19 09:25:09 +0800

    final_upload_19

--- boml/__init__.py
+++ boml/__init__.py
@@ -1,14 +1,13 @@
 try:
-
     from boml.load_data.experiment import BMLExperiment
     from boml.load_data import meta_mini_imagenet, meta_omniglot, OmniglotMetaDataset,\
-        ImageNetMetaDataset, mnist, read_dataset, augment_dataset
-    from boml.setup_model.BOMLNetMetaInitV1 import BOMLNetMiniMetaInitV1, BOMLNetOmniglotMetaInitV1
-    from boml.setup_model.BOMLNetMetaReprV1 import BOMLNetMiniMetaReprV1, BOMLNetOmniglotMetaReprV1
+        ImageNetMetaDataset, mnist
+    from boml.setup_model.BOMLNetMetaInitV1 import BOMLNetMetaInitV1, BOMLNetOmniglotMetaInitV1, BOMLNetMiniMetaInitV1
+    from boml.setup_model.BOMLNetMetaReprV1 import BOMLNetMetaReprV1, BOMLNetMiniMetaReprV1, BOMLNetOmniglotMetaReprV1
     from boml.setup_model.BOMLNetMetaInitV2 import BOMLNetOmniglotMetaInitV2, BOMLNetMiniMetaInitV2
-    from boml.setup_model.BOMLNetMetaReprV2 import BOMLNetMiniMetaReprV2, BOMLNetOmniglotMetaReprV2
-    from boml.setup_model.BOMLNet import BOMLNetFeedForward, BOMLNet
-    from boml.optimizer import BOMLOptAdam, BOMLOptMomentum, BOMLOptSGD
+    from boml.setup_model.BOMLNetMetaReprV2 import BOMLNetOmniglotMetaReprV2, BOMLNetMiniMetaReprV2
+    from boml.setup_model.BOMLNet import BOMLNetFeedForward,BOMLNet
+    from boml.optimizer import BOMLOptSGD, BOMLOptMomentum, BOMLOptAdam
     from boml.boml_optimizer import BOMLOptimizer
     from boml.extension import get_outerparameter
     from boml import utils
