23d949104512c6ef48510535e91bf62365d73f6e
bmlsoc <bml_soc@163.com>
2020-09-10 21:33:40 +0800

    final_commission

--- test_script/test_meta_init.py
+++ test_script/test_meta_init.py
@@ -15,7 +15,7 @@ map_dict = {'omniglot': boml.meta_omniglot, 'miniimagenet': boml.meta_mini_image
 
 
 def build(metasets, learn_lr, lr0, MBS, T, mlr0,mlr_decay=1.e-5, process_fn=None, method='MetaInit',inner_method='Simple', outer_method='Simple',
-          use_T=False, use_Warp=False, first_order=False, warp_labmda=1.0):
+          use_T=False, use_Warp=False, first_order=False, warp_lambda=1.0):
 
     exs = [dl.BOMLExperiment(metasets) for _ in range(MBS)]
 
@@ -36,7 +36,7 @@ def build(metasets, learn_lr, lr0, MBS, T, mlr0,mlr_decay=1.e-5, process_fn=None
                                          first_order=first_order)
         ex.errors['validation'] = boml.utils.cross_entropy(pred=ex.model.re_forward(ex.x_).out, label=ex.y_, method=method)
         boml_ho.ul_problem(outer_objective=ex.errors['validation'], meta_learning_rate=mlr0, inner_grad=optim_dict,
-                            outer_objective_optimizer=args.outer_opt,mlr_decay=mlr_decay, warp_lambda=warp_labmda,
+                            outer_objective_optimizer=args.outer_opt,mlr_decay=mlr_decay, warp_lambda=warp_lambda,
                             meta_param=tf.get_collection(boml.extension.GraphKeys.METAPARAMETERS))
     meta_learner = boml_ho.meta_model
     meta_learning_rate = boml_ho.meta_learning_rate
@@ -51,7 +51,7 @@ def build(metasets, learn_lr, lr0, MBS, T, mlr0,mlr_decay=1.e-5, process_fn=None
 
 
 # training and testing function
-def train_and_test(metasets, name_of_exp,method, inner_method, outer_method, use_T=False, use_Warp=False, warp_labmda=1.0,
+def train_and_test(metasets, name_of_exp,method, inner_method, outer_method, use_T=False, use_Warp=False, warp_lambda=1.0,
                    first_order=False, reptile=False, logdir='logs/', seed=None, lr0=0.04, learn_lr=False,
                    learn_alpha=False, learn_alpha_itr=False, learn_st=False,
                    mlr0=0.001, mlr_decay=1.e-5, alpha_decay=1.e-5, T=5, resume=True, MBS=4, n_meta_iterations=5000,
@@ -75,7 +75,7 @@ def train_and_test(metasets, name_of_exp,method, inner_method, outer_method, use
     copyfile(executing_file_path, os.path.join(exp_dir, executing_file_path.split('/')[-1]))
 
     exs, boml_ho, saver = build(metasets, learn_lr, lr0,MBS, T, mlr0,mlr_decay, process_fn,
-                                 method, inner_method, outer_method, use_T, use_Warp, first_order,warp_labmda)
+                                 method, inner_method, outer_method, use_T, use_Warp, first_order,warp_lambda)
 
     sess = tf.Session(config=boml.utils.set_gpu())
 
@@ -87,7 +87,7 @@ def train_and_test(metasets, name_of_exp,method, inner_method, outer_method, use
 
 
 # training and testing function
-def build_and_test(metasets, exp_dir,method, inner_method, outer_method, use_T=False,use_Warp=False,warp_labmda=1.0, first_order=False,
+def build_and_test(metasets, exp_dir,method, inner_method, outer_method, use_T=False,use_Warp=False,warp_lambda=1.0, first_order=False,
                    seed=None, lr0=0.04, T=5, MBS=4,
                    process_fn=None, n_test_episodes=600, iterations_to_test=list(range(100000))):
     params = locals()
@@ -103,7 +103,7 @@ def build_and_test(metasets, exp_dir,method, inner_method, outer_method, use_T=F
     tf.set_random_seed(seed)
 
     exs, pybml_ho, saver = build(metasets, learn_lr, lr0, MBS, T, mlr0,
-                                 mlr_decay,process_fn,method, inner_method, outer_method, use_T,use_Warp, first_order,warp_labmda)
+                                 mlr_decay,process_fn,method, inner_method, outer_method, use_T,use_Warp, first_order,warp_lambda)
 
     sess = tf.Session(config=boml.utils.set_gpu())
 
@@ -128,7 +128,7 @@ def test_meta_init():
     if args.mode == 'train':
         train_and_test(metasets, exp_string,method=args.method, inner_method=args.inner_method,
                        outer_method=args.outer_method, use_T=args.use_T, first_order=args.first_order, logdir=logdir,
-                       seed=args.seed,use_Warp=args.use_Warp, warp_labmda=args.warp_lambda,
+                       seed=args.seed,use_Warp=args.use_Warp, warp_lambda=args.warp_lambda,
                        lr0=args.lr, learn_lr=args.learn_lr, mlr0=args.meta_lr, T=args.T,
                        resume=args.resume, MBS=args.meta_batch_size, n_meta_iterations=args.n_meta_iterations,
                        process_fn=process_fn, save_interval=args.save_interval, print_interval=args.print_interval,
